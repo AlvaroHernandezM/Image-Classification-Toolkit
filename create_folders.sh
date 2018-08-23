349bc2d8a0a80d37704dab73b78d50211a206159
AlvaroHernandezM <alvaro.hernandez01@uptc.edu.co>
2018-08-23 11:22:38 -0500

    update create folders

--- create_folders.sh
+++ create_folders.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir static/img/positive/
 mkdir static/img/negative/
+mkdir static/img/log/
+mkdir static/img/single_prediction/
 mkdir core/cnn/dataset/
 mkdir core/cnn/dataset/test_set/
 mkdir core/cnn/dataset/test_set/class_positive/
@@ -10,4 +12,5 @@ mkdir core/cnn/dataset/training_set/class_positive/
 mkdir core/cnn/dataset/training_set/class_negative/
 mkdir core/image_retraining/dataset/
 mkdir core/image_retraining/models/
+mkdir core/image_retraining/tensorflow/
 mkdir core/svm_knn_bpnn/dataset/
